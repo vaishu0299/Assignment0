ce75cb064bea1b0bde11ab7238022b70bf0144b9
Vaishnavi Sai <vaishnavisai@Vaishnavis-MacBook-Air.local>
2022-05-17 19:56:08 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+class NewtonApproximation {
+    public static void main(String[] args) {
+        // Newton's Approximation to solve sin(x) = x
+        double x = 1.0;
+        int left = 200;
+        for (; left > 0; left--) {
+            final double y = Math.sin(x) - x;
+            if (Math.abs(y) < 1E-7) {
+                System.out.println("the solution to sin(x)=x is: " + x);
+                System.exit(0);
+            }
+            x = x - y / (Math.cos(x) - 1);
+        }
+    }
+}
+
